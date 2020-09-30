df4b87c73d3f5ad45fe0e7181cef6be476b1ee07
Yung Han Jeong <yungh.jeong@gmail.com>
2020-09-30 18:34:11 -0400

    made a presenation notebook

--- src/tools.py
+++ src/tools.py
@@ -38,3 +38,45 @@ def common_titles(title_list1: list, title_list2: list):
 #Output: 425112413 
 def dollar_str_to_num(num_str: str):
     return int("".join(digit for digit in num_str if digit.isnumeric()))
+
+#get the date_string and check the first string for month
+def get_month(date_string):
+    month = date_string.split()[0]
+    
+    if month == "Jan":
+        num_month = 1
+    elif month == "Feb":
+        num_month = 2
+    elif month == "Mar":
+        num_month = 3
+    elif month == "Apr":
+        num_month = 4
+    elif month == "May":
+        num_month = 5
+    elif month == "Jun":
+        num_month = 6
+    elif month == "Jul":
+        num_month = 7
+    elif month == "Aug":
+        num_month = 8
+    elif month == "Sep":
+        num_month = 9
+    elif month == "Oct":
+        num_month = 10
+    elif month == "Nov":
+        num_month = 11
+    else:
+        num_month = 12
+    
+    return num_month
+
+#assign season values depending on month
+def get_season(month_num):
+    if month_num in [12,1,2]:
+        return "Winter"
+    elif month_num in [3,4,5]:
+        return "Spring"
+    elif month_num in [6,7,8]:
+        return "Summer"
+    else:
+        return "Fall"
